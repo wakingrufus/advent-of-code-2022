cafa26a1df4e8e81073af19d8c5b5a7de21e90bd
wakingrufus <wakingrufus@gmail.com>
2022-12-02 11:04:26 -0600

    Day 2 clean up
    Day 2 Part 1 Time: 4 ms
    Day 2 Part 2 Time: 3 ms

--- src/main/kotlin/com/github/wakingrufus/aoc/Day2.kt
+++ src/main/kotlin/com/github/wakingrufus/aoc/Day2.kt
@@ -4,8 +4,7 @@ class Day2 : AocDay<Int>(2) {
 
     override fun part1(input: List<String>): Int {
         val rounds = input.map {
-            val tokens = it.split(" ")
-            Pair(Shape.fromLetter(tokens[0].first()), Shape.fromLetter(tokens[1].first()))
+            Shape.fromLetter(it[0]) to Shape.fromLetter(it[2])
         }
         return rounds.sumOf { scoreRound(it) + it.second.points }
     }
@@ -32,9 +31,8 @@ class Day2 : AocDay<Int>(2) {
 
     override fun part2(input: List<String>): Int {
         val rounds = input.map {
-            val tokens = it.split(" ")
-            val otherMove = Shape.fromLetter(tokens[0].first())
-            Pair(otherMove, chooseShape(otherMove, Outcome.fromLetter(tokens[1].first())))
+            val otherMove = Shape.fromLetter(it[0])
+            otherMove to chooseShape(otherMove, Outcome.fromLetter(it[2]))
         }
         return rounds.sumOf { scoreRound(it) + it.second.points }
     }
@@ -59,27 +57,9 @@ fun chooseShape(otherShape: Day2.Shape, outcome: Day2.Outcome): Day2.Shape {
 
 fun scoreRound(round: Pair<Day2.Shape, Day2.Shape>): Int {
     return when (round.first) {
-        round.second -> {
-            3
-        }
-
-        Day2.Shape.ROCK -> if (round.second == Day2.Shape.PAPER) {
-            6
-        } else {
-            0
-        }
-
-        Day2.Shape.PAPER -> if (round.second == Day2.Shape.SCISSORS) {
-            6
-        } else {
-            0
-        }
-
-        else ->  // SCISSORS
-            if (round.second == Day2.Shape.ROCK) {
-                6
-            } else {
-                0
-            }
+        round.second -> 3
+        Day2.Shape.ROCK -> if (round.second == Day2.Shape.PAPER) 6 else 0
+        Day2.Shape.PAPER -> if (round.second == Day2.Shape.SCISSORS) 6 else 0
+        Day2.Shape.SCISSORS -> if (round.second == Day2.Shape.ROCK) 6 else 0
     }
 }
